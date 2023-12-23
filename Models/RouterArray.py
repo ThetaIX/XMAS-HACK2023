0270a2dc2ddaad4a793dabb34a80400b26d069ac
ThetaIX <105356664+ThetaIX@users.noreply.github.com>
2023-12-23 20:32:20 +0700

    Update RouterArray.py

--- Models/RouterArray.py
+++ Models/RouterArray.py
@@ -12,3 +12,10 @@ class RouterArray(object):
             lines = s.readlines()
             for line in range(1,len(lines)):
                 self.routers.append(Router(lines[line]))
+
+    def getDF(self):
+        self.dd = {"geom" : []}
+        for a in self.routers:
+            self.dd['geom'].append(a.geom)
+        df = pd.DataFrame(self.dd)
+        return df
